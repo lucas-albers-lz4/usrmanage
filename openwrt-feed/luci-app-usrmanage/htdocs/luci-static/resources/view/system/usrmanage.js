46fb6277e791793f1250d0db17eeb4a65975b86f
Lucas Albers <lucas.b.albers@gmail.com>
2026-08-18 14:05:48 +0000

    chore: release v0.1.5
    Bump PKG_VERSION (both Makefiles) and APP_VERSION to v0.1.5.

--- openwrt-feed/luci-app-usrmanage/htdocs/luci-static/resources/view/system/usrmanage.js
+++ openwrt-feed/luci-app-usrmanage/htdocs/luci-static/resources/view/system/usrmanage.js
@@ -7,7 +7,7 @@
 'require poll';
 
 /* Keep in sync with openwrt-feed/luci-app-usrmanage/Makefile PKG_VERSION */
-const APP_VERSION = '0.1.4';
+const APP_VERSION = '0.1.5';
 
 const callList = rpc.declare({
 	object: 'usrmanage',
