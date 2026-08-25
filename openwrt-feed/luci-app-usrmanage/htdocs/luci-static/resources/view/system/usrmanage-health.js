e5553c82f53ebf9f1c5d7a79d05bd82c28409a74
Lucas Albers <lucas.b.albers@gmail.com>
2026-08-25 03:59:19 +0000

    chore: release v0.1.14
    Bump PKG_VERSION/APP_VERSION to 0.1.14; move LUCI_EXTRA_DEPENDS pin and health.js APP_VERSION mirror in lockstep.

--- openwrt-feed/luci-app-usrmanage/htdocs/luci-static/resources/view/system/usrmanage-health.js
+++ openwrt-feed/luci-app-usrmanage/htdocs/luci-static/resources/view/system/usrmanage-health.js
@@ -5,7 +5,7 @@
 'require ui';
 
 /* Keep in sync with openwrt-feed/luci-app-usrmanage/Makefile PKG_VERSION */
-const APP_VERSION = '0.1.13';
+const APP_VERSION = '0.1.14';
 
 const callHealth = rpc.declare({
 	object: 'usrmanage',
